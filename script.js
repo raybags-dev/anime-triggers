0dca93e506b1838e9c656e450b4e838077958a02
raymond <raymond.baguma@revinate.com>
2022-08-13 21:31:44 +0200

    added game frame emulation and sound effects

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 700;
+let canvasPosition = canvas.getBoundingClientRect();
 
-const circle1 = { x: 10, y: 10, radius: 300 };
-const circle2 = { x: 500, y: 500, radius: 150 }
+const explosions = [];
 
-let dx = circle2.x - circle1.x;
-let dy = circle2.y - circle1.y;
-let distance = Math.sqrt(dx * dx + dy * dy);
-let sumOfRadii = circle1.radius + circle2.radius;
+class Explossion {
+    constructor(x, y) {
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = './assets/boom.png';
+        this.frame = 0;
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = './Sound-effects/Fire impact 1.wav'
+    }
+    update() {
+        if (this.frame === 0) this.sound.play();
+        this.timer++;
+        if (this.timer % 2 === 0) {
+            this.frame++;
+        }
+    }
+    draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0,
+            this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.width / 2,
+            this.width, this.height);
+        ctx.restore();
+    }
 
-if (distance < sumOfRadii) {
-    //circle collides
-} else if (distance === sumOfRadii) {
-    //circle touch
-} else if (distance > sumOfRadii) {
-    //no collission
 }
+
+window.addEventListener('mousedown', createAnimation);
+//window.addEventListener('mousemove', createAnimation);
+
+
+function createAnimation(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explossion(positionX, positionY));
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        // remove images that have stopped animating 
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate)
+}
+animate();
